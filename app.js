d73268e7e876192879af1b88099cd0c9081121e3
Munyaradzi Makosa <munya@farmhutafrica.com>
2024-01-23 14:21:02 +0200

    fix index and fix token

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express();
 app.use(express.static("public"));
 app.listen(
   process.env.PORT || 3000, 
-	() => console.log("Server is running...")
+	() => console.log("Server is running..")
 );
